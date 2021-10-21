8c4e51122efbc9771d1e26d47a28b490c174e6b3
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-10-21 11:02:56 -0300

    Formatação do código

--- sources/display.c
+++ sources/display.c
@@ -1,3 +1,12 @@
+/**
+ * Trabalho 01 - Estrutura de Dados III
+ *
+ * Integrantes do grupo:
+ *
+ * Leonardo Hannas de Carvalho Santos  NUSP: 11800480
+ * Lucas Carvalho Freiberger Stapf     NUSP: 11800559
+ */
+
 #include <stdio.h>
 #include "../headers/display.h"
 #include "../headers/constants.h"
@@ -5,19 +14,19 @@
 /**
   - Function: showMessage
   - Description: Esta funcao printa na tela uma mensagem de acordo com o parametro passado.
-  - Parameters: 
-    - cod: codigo da mensagem.
-  - Returns: 
-    - cod
+  - Parameters:
+    - cod: Codigo da mensagem.
+  - Returns:
+    - cod: O proprio codigo passado
 */
 int showMessage(int cod) {
 
-    switch (cod)
-    {
+    switch(cod) {
+    
     case FILE_ERROR:
         printf("Falha no processamento do arquivo.");
         break;
-    
+
     case REGISTER_NOT_FOUND:
         printf("Registro inexistente.");
         break;
