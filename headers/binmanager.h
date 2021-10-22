c037a5964458c6a5f59febc4e8825fc7f5fdbf45
Lucas Stapf <62681897+LucasStapf@users.noreply.github.com>
2021-10-22 12:57:45 -0300

    Adição cabeçalho
    Adição do cabeçalho da função "statusFileBIN";

--- headers/binmanager.h
+++ headers/binmanager.h
@@ -18,12 +18,13 @@ void readHeaderRegisterBIN(FILE *f, HeaderRegister *rc);
 void writeDataRegisterBIN(FILE *f, DataRegister *dr);
 int readDataRegisterBIN(FILE *f, DataRegister *dr);
 int findDataRegistersBIN(FILE *f, DataRegister *dr);
-void deleteDataRegisterBIN(FILE *f, DataRegister *dr);
-void insertDataRegisterBIN(FILE *f, DataRegister *dr);
-void updateDataRegisterBIN(FILE *f, DataRegister *dr_busca, DataRegister *dr_alteracao);
+int deleteDataRegisterBIN(FILE *f, DataRegister *dr);
+int insertDataRegisterBIN(FILE *f, DataRegister *dr);
+int updateDataRegisterBIN(FILE *f, DataRegister *dr_busca, DataRegister *dr_alteracao);
 
 int createFileBIN(char *csvName, char *binName);
 int printFileBIN(char *binName); 
+int statusFileBIN(HeaderRegister hr);
 
 void fillWithTrash(FILE *f, int numBytes);
 long findAvailableSpaceRegister(FILE *f, LONG_8 topoDaLista, LONG_8 *byteAnterior, LONG_8 *byteProximo, long numBytes);
